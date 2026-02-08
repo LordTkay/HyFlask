1211a995e4e5c454910cb87bdbb7e51c201793d7
Tobias Wagner <tobawa2601@gmail.com>
2026-02-08 15:14:36 +0100

    Removed the example classes and renamed the Plugin-Class to HyFlaskPlugin and corrected the package path.

---
+++ src/main/java/de/lordtkay/hyflask/HyFlaskPlugin.java
@@ -0,0 +1,17 @@
+package de.lordtkay.hyflask;
+
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+import javax.annotation.Nonnull;
+
+public class HyFlaskPlugin extends JavaPlugin {
+
+    public HyFlaskPlugin(@Nonnull JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    protected void setup() {
+    }
+}
