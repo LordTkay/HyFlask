4aa81fdf7f18083fdd45f23517b26aad5962f556
Tobias Wagner <tobawa2601@gmail.com>
2026-02-08 15:54:56 +0100

    Suppressed the unused warning on the HyFlaskPlugin, because that is the entrypoint of the plugin itself and will be executed by Hytale.

--- src/main/kotlin/de/lordtkay/hyflask/HyFlaskPlugin.kt
+++ src/main/kotlin/de/lordtkay/hyflask/HyFlaskPlugin.kt
@@ -3,6 +3,7 @@ package de.lordtkay.hyflask
 import com.hypixel.hytale.server.core.plugin.JavaPlugin
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit
 
+@Suppress("unused")
 class HyFlaskPlugin(init: JavaPluginInit) : JavaPlugin(init) {
     override fun setup() {
     }
