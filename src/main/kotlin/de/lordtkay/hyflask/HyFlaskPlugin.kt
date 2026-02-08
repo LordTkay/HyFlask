ee6611c401ef7db99c8b356f02b17615ff2459c5
Tobias Wagner <tobawa2601@gmail.com>
2026-02-08 18:53:44 +0100

    docs: Added logging on the plugin setup, start and shutdown hooks

--- src/main/kotlin/de/lordtkay/hyflask/HyFlaskPlugin.kt
+++ src/main/kotlin/de/lordtkay/hyflask/HyFlaskPlugin.kt
@@ -1,10 +1,34 @@
 package de.lordtkay.hyflask
 
+import com.hypixel.hytale.logger.HytaleLogger
 import com.hypixel.hytale.server.core.plugin.JavaPlugin
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit
 
 @Suppress("unused")
 class HyFlaskPlugin(init: JavaPluginInit) : JavaPlugin(init) {
+
+    companion object {
+        private val logger = HytaleLogger.forEnclosingClass()
+        var instance: HyFlaskPlugin? = null
+            private set
+    }
+
+    init {
+        instance = this
+    }
+
     override fun setup() {
+        logger.atInfo().log("[$name] Setting up...")
+
+        logger.atInfo().log("[$name] Setup complete!")
+    }
+
+    override fun start() {
+        logger.atInfo().log("[$name] Started!")
+    }
+
+    override fun shutdown() {
+        logger.atInfo().log("[$name] Shut down")
+        instance = null
     }
 }
