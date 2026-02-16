786daabc0e39efc23e968f9b43274e69c2f70a92
Tobias Wagner <tobawa2601@gmail.com>
2026-02-16 20:59:33 +0100

    feature: Added asset type for a `FlaskEffect`
    Registered the new `FlaskEffect` asset in the asset store

--- src/main/kotlin/de/lordtkay/hyflask/HyFlaskPlugin.kt
+++ src/main/kotlin/de/lordtkay/hyflask/HyFlaskPlugin.kt
@@ -1,8 +1,11 @@
 package de.lordtkay.hyflask
 
+import com.hypixel.hytale.assetstore.map.IndexedAssetMap
 import com.hypixel.hytale.logger.HytaleLogger
+import com.hypixel.hytale.server.core.asset.HytaleAssetStore
 import com.hypixel.hytale.server.core.plugin.JavaPlugin
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit
+import de.lordtkay.hyflask.effect.asset.FlaskEffect
 
 @Suppress("unused")
 class HyFlaskPlugin(init: JavaPluginInit) : JavaPlugin(init) {
@@ -20,7 +23,26 @@ class HyFlaskPlugin(init: JavaPluginInit) : JavaPlugin(init) {
     override fun setup() {
         logger.atInfo().log("[$name] Setting up...")
 
+        registerAssetStores()
+
         logger.atInfo().log("[$name] Setup complete!")
+    }
+
+
+    private fun registerAssetStores() {
+        val flaskEffectAssetStore = HytaleAssetStore
+            .builder(
+                String::class.java,
+                FlaskEffect::class.java,
+                IndexedAssetMap<String, FlaskEffect>()
+            )
+            .setCodec(FlaskEffect.CODEC)
+            .setPath(FlaskEffect.ASSET_PATH)
+            .setKeyFunction { asset -> asset.id }
+            .setReplaceOnRemove { id -> FlaskEffect(id) }
+            .build()
+
+        assetRegistry.register(flaskEffectAssetStore)
     }
 
     override fun start() {
