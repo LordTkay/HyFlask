6239c7adda5d81596dde227b8cc30ee5916959f2
Tobias Wagner <tobawa2601@gmail.com>
2026-02-18 21:15:21 +0100

    feature: Added a collection for the effects commands
    Created a new class, which will hold all subcommand for the flask effects. Registered it in the HyFlaskCommandCollection.

---
+++ src/main/kotlin/de/lordtkay/hyflask/effect/command/EffectsCommandCollection.kt
@@ -0,0 +1,10 @@
+package de.lordtkay.hyflask.effect.command
+
+import com.hypixel.hytale.server.core.command.system.basecommands.AbstractCommandCollection
+
+class EffectsCommandCollection : AbstractCommandCollection("effects", "server.hyflask.commands.effects") {
+
+    init {
+    }
+
+}
