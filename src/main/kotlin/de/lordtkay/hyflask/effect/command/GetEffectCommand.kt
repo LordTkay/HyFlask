b836e8db45b6884972ca4db881f8d164f2183353
Tobias Wagner <tobawa2601@gmail.com>
2026-02-18 23:15:08 +0100

    feature: Added a command that displays all learned and active effects
    Added a command that displays all active and learned effects of the given player. Registered the command in the EffectsCommandCollection and added the new translation texts in the server.lang.

---
+++ src/main/kotlin/de/lordtkay/hyflask/effect/command/GetEffectCommand.kt
@@ -0,0 +1,40 @@
+package de.lordtkay.hyflask.effect.command
+
+import com.hypixel.hytale.component.Ref
+import com.hypixel.hytale.component.Store
+import com.hypixel.hytale.logger.HytaleLogger
+import com.hypixel.hytale.server.core.Message
+import com.hypixel.hytale.server.core.command.system.CommandContext
+import com.hypixel.hytale.server.core.command.system.basecommands.AbstractTargetPlayerCommand
+import com.hypixel.hytale.server.core.universe.PlayerRef
+import com.hypixel.hytale.server.core.universe.world.World
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore
+import de.lordtkay.hyflask.effect.component.FlaskEffectComponent
+
+class GetEffectCommand : AbstractTargetPlayerCommand("get", "server.hyflask.commands.effects.get") {
+
+    companion object {
+        private var logger = HytaleLogger.forEnclosingClass()
+    }
+
+    override fun execute(
+        commandContext: CommandContext,
+        ref: Ref<EntityStore?>?,
+        ref1: Ref<EntityStore?>,
+        playerRef: PlayerRef,
+        world: World,
+        store: Store<EntityStore?>
+    ) {
+        val flaskEffectComponent =
+            store.ensureAndGetComponent(playerRef.reference!!, FlaskEffectComponent.componentType)
+
+        val activeEffects = flaskEffectComponent.activeEffects.mapNotNull { fetchEffect(logger, playerRef, it) }
+        val learnedEffects = flaskEffectComponent.learnedEffects.mapNotNull { fetchEffect(logger, playerRef, it) }
+
+        val message = Message.translation("server.hyflask.commands.effects.get.success")
+            .param("activeEffects", activeEffects.joinToString("\n") { "- $it" })
+            .param("learnedEffects", learnedEffects.joinToString("\n") { "- $it" })
+
+        playerRef.sendMessage(message)
+    }
+}
