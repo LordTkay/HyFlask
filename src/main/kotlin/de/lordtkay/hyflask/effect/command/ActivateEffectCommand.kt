32add8d5d4da9cae7846ea97b40c3ed187ef848b
Tobias Wagner <tobawa2601@gmail.com>
2026-02-18 22:18:27 +0100

    feature: Added a command that allows activating learned flask effects
    Added a command that lets you activate a flask effect that was already learned by entering the flask effect asset ID. Registered the command in the EffectsCommandCollection and added the new translation texts in the server.lang.

---
+++ src/main/kotlin/de/lordtkay/hyflask/effect/command/ActivateEffectCommand.kt
@@ -0,0 +1,68 @@
+package de.lordtkay.hyflask.effect.command
+
+import com.hypixel.hytale.component.Ref
+import com.hypixel.hytale.component.Store
+import com.hypixel.hytale.logger.HytaleLogger
+import com.hypixel.hytale.server.core.Message
+import com.hypixel.hytale.server.core.command.system.CommandContext
+import com.hypixel.hytale.server.core.command.system.arguments.types.ArgTypes
+import com.hypixel.hytale.server.core.command.system.basecommands.AbstractTargetPlayerCommand
+import com.hypixel.hytale.server.core.universe.PlayerRef
+import com.hypixel.hytale.server.core.universe.world.World
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore
+import de.lordtkay.hyflask.effect.component.FlaskEffectComponent
+
+class ActivateEffectCommand : AbstractTargetPlayerCommand("activate", "server.hyflask.commands.effects.activate") {
+
+    companion object {
+        private var logger = HytaleLogger.forEnclosingClass()
+    }
+
+    private val effectIdArg = this.withRequiredArg(
+        "effectId",
+        "server.hyflask.commands.effects.activate.effectId",
+        ArgTypes.STRING
+    )
+
+    override fun execute(
+        commandContext: CommandContext,
+        ref: Ref<EntityStore?>?,
+        ref1: Ref<EntityStore?>,
+        playerRef: PlayerRef,
+        world: World,
+        store: Store<EntityStore?>
+    ) {
+        val flaskEffectComponent =
+            store.ensureAndGetComponent(playerRef.reference!!, FlaskEffectComponent.componentType)
+
+        val effectId = effectIdArg.get(commandContext)
+        val assetName = fetchEffect(logger, playerRef, effectId) ?: return
+
+        val wasActivated = flaskEffectComponent.activateEffect(effectId)
+
+        if (!wasActivated) {
+            if (effectId in flaskEffectComponent.learnedEffects) {
+                val message = Message.translation("server.hyflask.commands.effects.activate.alreadyActive")
+                    .param("name", assetName)
+                playerRef.sendMessage(message)
+
+                logger.atInfo()
+                    .log("Player '${playerRef.username}' tried to activate flask effect '${assetName}' but it was already active")
+            } else {
+                val message = Message.translation("server.hyflask.commands.effects.activate.notLearned")
+                    .param("name", assetName)
+                playerRef.sendMessage(message)
+
+                logger.atInfo()
+                    .log("Player '${playerRef.username}' tried to activate flask effect '${assetName}' but it was not learned yet")
+            }
+            return
+        }
+
+        val message = Message.translation("server.hyflask.commands.effects.activate.success")
+            .param("name", assetName)
+        playerRef.sendMessage(message)
+
+        logger.atInfo().log("Player '${playerRef.username}' activated flask effect '${assetName}'")
+    }
+}
