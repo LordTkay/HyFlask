0b3eff24dea48639132057ed5f3d41f7c8a9362b
Tobias Wagner <tobawa2601@gmail.com>
2026-03-08 22:26:38 +0100

    feature: Added "Get" command for the capacity that displays the current and max capacity value of the target player
    Created the new command class and registered it in the plugin

---
+++ src/main/kotlin/de/lordtkay/hyflask/capacity/command/GetCapacityCommand.kt
@@ -0,0 +1,50 @@
+package de.lordtkay.hyflask.capacity.command
+
+import com.hypixel.hytale.component.Ref
+import com.hypixel.hytale.component.Store
+import com.hypixel.hytale.logger.HytaleLogger
+import com.hypixel.hytale.server.core.Message
+import com.hypixel.hytale.server.core.command.system.CommandContext
+import com.hypixel.hytale.server.core.command.system.basecommands.AbstractTargetPlayerCommand
+import com.hypixel.hytale.server.core.universe.PlayerRef
+import com.hypixel.hytale.server.core.universe.world.World
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore
+import de.lordtkay.hyflask.enumeration.HyFlaskEntityStat
+import de.lordtkay.hyflask.utility.command.EntityStatUtility
+
+class GetCapacityCommand(
+    parentTranslationKey: String,
+    private val translationKey: String = "$parentTranslationKey.get"
+) : AbstractTargetPlayerCommand("get", translationKey) {
+
+    companion object {
+        private val logger = HytaleLogger.forEnclosingClass()
+    }
+
+    override fun execute(
+        commandContext: CommandContext,
+        sourceRef: Ref<EntityStore?>?,
+        ref: Ref<EntityStore?>,
+        playerRef: PlayerRef,
+        world: World,
+        store: Store<EntityStore?>
+    ) {
+        val result = EntityStatUtility.get(
+            ref,
+            store,
+            HyFlaskEntityStat.CAPACITY,
+        )
+
+        val message = when (result) {
+            is EntityStatUtility.Result.ComponentMissing ->
+                Message.translation("server.hyflask.commands.error")
+
+            is EntityStatUtility.Result.Success ->
+                Message.translation("$translationKey.success")
+                    .param("current", result.amount)
+                    .param("max", result.max)
+        }
+
+        commandContext.sendMessage(message)
+    }
+}
